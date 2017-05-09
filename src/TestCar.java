858c3ad51e3509f0131ce41f488552b395ddf4f9
fvales <freedavales28@gmail.com>
2017-05-09 12:31:02 +0530

    This is my second commit... Made changes to TestCar.java

--- src/TestCar.java
+++ src/TestCar.java
@@ -5,10 +5,17 @@ import org.junit.Test;
 public class TestCar {
 
 	@Test
-	public void test() {
+	public void testForFail() {
 		Car c = new Car();
 		c.setEngineCapacity(120);
 		assertEquals(200,c.getEngineCapacity());
+	}
+	
+	@Test
+	public void testForSuccess() {
+		Car c = new Car();
+		c.setEngineCapacity(120);
+		assertEquals(120,c.getEngineCapacity());
 	}
 
 }
